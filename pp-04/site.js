a5a61b13a317674c58d773519ed46614b8941576
kchenzhang <kchenzhang@hawk.iit.edu>
2020-11-16 14:15:44 -0600

    Rewrite function for step 2

--- pp-04/site.js
+++ pp-04/site.js
@@ -3,7 +3,7 @@
   call outputs the correct value for x (5). You must still use
   `x` as the variable inside the function, however.
 */
-
+/*
 x = 5;
 
 function double(num) {
@@ -13,7 +13,7 @@ function double(num) {
 
 double(6);
 console.log('The value of x is', x, '-- it should be 5.');
-
+*/
 /*
   Step 2. Rewrite the JavaScript `double()` function above so that
   it does not pollute the global namespace: that is, neither its
@@ -23,7 +23,17 @@ console.log('The value of x is', x, '-- it should be 5.');
   Be sure to comment out the original `double()` function so you
   can accurately test your work.
 */
+function main(){
+  var x =5;
+  function double(num){
+    var x =num * 2;
+    console.log('The value of x doubled is:',x);
+    return x;
+  }
+  double(6);
+  console.log('The value of x is', x,'--it should be 5.')
 
+}
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
